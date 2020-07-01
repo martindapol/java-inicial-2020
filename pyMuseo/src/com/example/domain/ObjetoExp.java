d072cbfa19a10ed400d8fb3ced27099aabd83547
martindapol <martindapol@gmail.com>
2020-07-01 17:58:21 -0300

    py Museo para completar

---
+++ pyMuseo/src/com/example/domain/ObjetoExp.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.domain;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author MARTIN
+ */
+public abstract class ObjetoExp {
+
+    private int cod;
+    private String nombre;
+    private int anio;
+
+    public ObjetoExp() {
+    }
+
+    public ObjetoExp(int cod, String nombre, int anio) {
+        this.cod = cod;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: " + cod + "| Nombre: " + nombre + "| Año: " + anio;
+    }
+
+   
+
+    
+}
